7fcad05ce390a34627e35625f259880e74ec84a1
SeanW <sean.walmer@outlook.com>
2020-05-17 00:16:08 -0400

    Added prompts to start the loop, all three employee types (that are pushed to employee array), and the ability to add another employee before ending.

--- Develop/app.js
+++ Develop/app.js
@@ -10,8 +10,287 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const employees = []
+const employees = [];
 
+var teamName = ""
+
+function startBuild() {
+    //welcome and prompt for team name
+    const questions = [
+        {
+            type: 'input',
+            name: 'teamName',
+            message: 'please enter your team or division name',
+            validate: function (value) {
+                if (value === '') {
+                    return 'please enter a name';
+                } else {
+                    return true;
+                };
+            },
+        },
+    ]
+    inquirer.prompt(questions).then(answers => {
+        console.log('\n')
+        teamName = answers.teamName
+        chooseType();
+    });
+};
+
+function chooseType() {
+    // what type of employee will you be adding?
+    const questions = [
+        {
+            type: 'list',
+            name: 'type',
+            message: 'What type of employee do you want to add?',
+            choices: ['Manager', 'Engineer', 'Intern']
+        },
+    ]
+    // Run the employee build type based on response
+    inquirer.prompt(questions).then(answers => {
+        if(answers.type === 'Manager'){
+            console.log("Adding a manager...")
+            addManager();
+        }else if(answers.type === 'Engineer'){
+            console.log("Adding an engineer...")
+            addEngineer();
+        }else{
+            console.log("Adding an intern...")
+            addIntern();
+        }
+    });
+};
+
+function addEngineer() {
+    // prompt for needed information
+    const questions = [
+        {
+            type: 'input',
+            name: 'name',
+            message: `Engineer's Name: `,
+            validate: function (value) {
+                if (value === '') {
+                    return 'please enter a name';
+                } else {
+                    return true;
+                };
+            },
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: `Engineer's id: `,
+            validate: function (value) {
+                if (value === '') {
+                    return 'please enter an id';
+                } else {
+                    return true;
+                };
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `Engineer's email: `,
+            validate: function(value) {
+                var pass = value.match(
+                  /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i
+                );
+                if (pass) {
+                  return true;
+                };
+          
+                return 'Please enter a valid email address';
+            }
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: `Engineer's github: `,
+            validate: function (value) {
+                if (value === '') {
+                    return 'please enter a github id';
+                } else {
+                    return true;
+                };
+            },
+        },
+    ]
+    inquirer.prompt(questions).then(answers => {
+        const engi = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        // console.log(engi);
+        employees.push(engi);
+        console.log(`${engi.name} has been added as an engineer.`);
+        loopPrompt();
+    });
+    // create engineer and push to employee array
+    // ask if there are more employees to add
+    //chooseType if yes - start render if no
+};
+
+function addManager() {
+    // prompt for needed information
+    const questions = [
+        {
+            type: 'input',
+            name: 'name',
+            message: `Manager's Name: `,
+            validate: function (value) {
+                if (value === '') {
+                    return 'please enter a name';
+                } else {
+                    return true;
+                };
+            },
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: `Manager's id: `,
+            validate: function (value) {
+                if (value === '') {
+                    return 'please enter an id';
+                } else {
+                    return true;
+                };
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `Manager's email: `,
+            validate: function(value) {
+                var pass = value.match(
+                  /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i
+                );
+                if (pass) {
+                  return true;
+                };
+          
+                return 'Please enter a valid email address';
+            }
+        },
+        {
+            type: 'input',
+            name: 'phone',
+            message: `Manager's office phone number: `,
+            validate: function (value) {
+                if (value === '') {
+                    return 'please enter a phone number';
+                } else {
+                    return true;
+                };
+            },
+        },
+    ]
+    inquirer.prompt(questions).then(answers => {
+        const manager = new Manager(answers.name, answers.id, answers.email, answers.phone);
+        // console.log(manager);
+        employees.push(manager);
+        console.log(`${manager.name} has been added as a manager.`);
+        loopPrompt();
+    });
+    // create manager and push to employee array
+    // ask if there are more employees to add
+    //chooseType if yes - start render if no
+};
+
+function addIntern() {
+    // prompt for needed information
+    const questions = [
+        {
+            type: 'input',
+            name: 'name',
+            message: `Intern's Name: `,
+            validate: function (value) {
+                if (value === '') {
+                    return 'please enter a name';
+                } else {
+                    return true;
+                };
+            },
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: `Intern's id: `,
+            validate: function (value) {
+                if (value === '') {
+                    return 'please enter an id';
+                } else {
+                    return true;
+                };
+            },
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: `Intern's email: `,
+            validate: function(value) {
+                var pass = value.match(
+                  /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i
+                );
+                if (pass) {
+                  return true;
+                };
+          
+                return 'Please enter a valid email address';
+            }
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: `Intern's School: `,
+            validate: function (value) {
+                if (value === '') {
+                    return 'please enter a school name';
+                } else {
+                    return true;
+                };
+            },
+        },
+    ]
+    inquirer.prompt(questions).then(answers => {
+        const intern = new Intern (answers.name, answers.id, answers.email, answers.school);
+        // console.log(intern);
+        employees.push(intern);
+        console.log(`${intern .name} has been added as an intern.`);
+        loopPrompt();
+    });
+    // create intern and push to employee array
+    // ask if there are more employees to add
+    //chooseType if yes - start render if no
+};
+
+function startRender() {
+    // ask if list of employees is everyone
+    //if no chooseType if yes render
+    // write file to output
+};
+
+function loopPrompt(){
+    // ask if they have anyone else to add
+    const questions = [
+        {
+            type: 'list',
+            name: 'loop',
+            message: `Do you have any more employees to add?`,
+            choices: ['yes', 'no']
+        },
+    ]
+    //either cycle back to chooseType or start render
+    inquirer.prompt(questions).then(answers => {
+        if(answers.loop === 'yes'){
+            chooseType();
+        }else{
+            startRender();
+        };
+        console.log(employees)
+    });
+}
+
+startBuild();
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
